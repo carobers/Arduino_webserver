ee06d92a1e39d3c0e67d8cb657da1a13f429a964
Cody Roberson <carobers@asu.edu>
2024-02-29 23:29:09 -0700

    added project

---
+++ arduino_webserver/arduino_webserver.ino
@@ -0,0 +1,86 @@
+/**
+ * Post request example
+ * This isn't clean or error resistant, but it is a good demonstration of hosting a webpage off of an SD card and reading a post request
+ * From the user. This is an alternative method for controlling the device, rather than using the serial monitor.
+ * 
+ * @author: Cody Roberson
+ * 
+ * @date: 2024-02-29
+ * 
+ * Referenced Material
+ * https://www.arduino.cc/reference/en/libraries/ethernet/client.read/
+ * https://www.arduino.cc/reference/en/language/functions/communication/stream/
+ * https://www.arduino.cc/reference/en/language/functions/communication/stream/streamreadstringuntil/
+ * https://github.com/bblanchon/ArduinoJson/blob/7.x/examples/JsonHttpClient/JsonHttpClient.ino
+ * https://www.eso.org/~ndelmott/ascii.html
+ * https://www.freecodecamp.org/news/javascript-post-request-how-to-send-an-http-post-request-in-js/
+ * https://docs.arduino.cc/learn/programming/sd-guide/
+ * 
+ * 
+*/
+#define SERVE_PAGE
+#include <Arduino.h>
+
+#include "request_handler.h"
+
+uint8_t mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xEF, 0xED};
+IPAddress ip(192, 168, 200, 177);
+uint8_t dns[] = {8, 8, 8, 8};
+uint8_t gateway[] = {192, 168, 200, 1};
+uint8_t subnet[] = {255, 255, 255, 0};
+
+EthernetServer server = EthernetServer(80);
+
+File myFile;
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial){};
+
+    // Chipselect
+    pinMode(10, OUTPUT); //Ethernet
+    digitalWrite(10, HIGH); //PS - Active Low
+    pinMode(9, OUTPUT); //SD
+    digitalWrite(9, HIGH); //PS - Active Low
+    pinMode(8, OUTPUT); //eth reset
+    digitalWrite(8, HIGH); //active low
+    Ethernet.begin(mac, ip, gateway, subnet, dns);
+    Ethernet.init(10);
+    server.begin();
+    pinMode(6, INPUT);
+
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(9)) {
+        Serial.println("initialization failed!");
+        while (1);
+    }
+    Serial.println("initialization done.");
+}
+
+String mycallback(http_request_t req) {
+    String resp = "";
+    JsonDocument doc;
+    doc["sensor"] = "gps";
+    doc["time"] = 1351824120;
+    doc["data"][0] = 48.756080;
+    doc["data"][1] = 2.302038;
+
+    serializeJson(doc, resp);
+    return resp;
+}
+
+void loop(){
+    EthernetClient client = server.available();
+    char c;
+    http_request_t req;
+
+    if(client){
+        JsonDocument doc;
+        Serial.println("client connected");
+        req = handle_client_request(client, mycallback);
+        Serial.println(req.post_data);
+        deserializeJson(doc, req.post_data);
+        client.stop();
+        
+    }
+}
---
+++ arduino_webserver/request_handler.h
@@ -0,0 +1,93 @@
+#include <Ethernet.h>
+#include <SD.h>
+#include <ArduinoJson.h>
+
+
+typedef struct http_request_t {
+    String method;
+    String path;
+    String protocol;
+    String post_data;
+} http_request_t;
+
+int serve_webpage_from_sdcard(EthernetClient client, String filename) {
+    File webpage = SD.open(filename);
+    if (!webpage) {
+        Serial.println("File not found");
+        return 0;
+    }
+    char c;
+    client.println("HTTP/1.1 200 OK\rContent-Type: text/html\rConnection: close");
+    client.println();
+    if (webpage) {
+        while (webpage.available()){
+            c = webpage.read();
+            client.print(c);
+        }
+    }
+    webpage.close();
+    return 1;
+}
+
+
+int serve_page(http_request_t req, EthernetClient client) {
+    // Serial.println("serve_page() called");
+    // Serial.println(req.path);
+    if(req.path == "/") {
+        Serial.println("Serving index.htm");
+        return serve_webpage_from_sdcard(client, "index.htm");
+    } else if(req.path != "") {
+        Serial.println("Serving " + req.path);
+        String path = req.path;
+        path.remove(0, 1); // remove leading '/'
+        return serve_webpage_from_sdcard(client, req.path);
+    } else {
+        return 0;
+    }
+}
+
+
+
+http_request_t handle_client_request(EthernetClient client, String (*post_callback)(http_request_t)) {
+    http_request_t req;
+
+    String request_firstline = client.readStringUntil('\n');
+
+    if(request_firstline.indexOf("GET") != -1) {
+        request_firstline.replace("GET ", "");
+        request_firstline.replace(" HTTP/1.1", "");
+        req.method = "GET";
+        request_firstline.trim();
+        req.path = request_firstline;
+        req.protocol = "HTTP/1.1";
+
+        while (client.available())
+            client.read();
+        
+        serve_page(req, client);
+
+    } else if(request_firstline.indexOf("POST") != -1) {
+        request_firstline.replace("POST ", "");
+        request_firstline.replace(" HTTP/1.1", "");
+        req.method = "POST";
+        request_firstline.trim();
+        req.path = request_firstline;
+        req.protocol = "HTTP/1.1";
+
+        if (client.find("\r\n\r\n")) {
+            String post_data = client.readString();
+            req.post_data = post_data;
+            
+            client.println("HTTP/1.1 200 OK");
+            client.println();
+            if (post_callback != NULL) {
+                client.println(post_callback(req));
+            }
+            
+        }
+    } else {
+        Serial.println("Unknown request");
+    }
+    return req;
+}
+
